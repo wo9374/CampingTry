384f5102ffa8315ba8d727b7a527fa5e7ae40dd7
june <hwijune4011@gmail.com>
2020-05-01 20:51:14 +0900

    20200501 휘준 - 구글맵 추가(4번 창)
    수정 사항
    구글맵 추가 및 로그인 미리 쳐둠

--- app/src/main/java/com/example/mylogin/Frag4.java
+++ app/src/main/java/com/example/mylogin/Frag4.java
@@ -1,5 +1,6 @@
 package com.example.mylogin;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,19 +13,92 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.util.ArrayList;
 
-public class Frag4 extends Fragment {
+public class Frag4 extends Fragment implements OnMapReadyCallback {
 
-    private View view;
+    private MapView mapView = null;
 
-
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag4, container, false);
+        View view = inflater.inflate(R.layout.frag4, container, false);
+
+        mapView = (MapView) view.findViewById(R.id.googleMap);
+        mapView.getMapAsync(this);
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (mapView != null)
+        {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng YJP = new LatLng(35.896274, 128.621879);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(YJP);
+        markerOptions.title("영진전문대학교");
+        markerOptions.snippet("대구광역시 북구 복현2동 복현로 35");
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(YJP));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
     }
 }
