b5c754f80b133e5f54d0b3beaaf983d0390b7aec
june <hwijune4011@gmail.com>
2020-05-18 16:39:02 +0900

    200518-3번 프래그먼트 제작중
    수정 사항
    드로어메뉴 수정 및 거래 제작중

--- app/src/main/java/com/example/mylogin/Shop/ShopFrag.java
+++ app/src/main/java/com/example/mylogin/Shop/ShopFrag.java
@@ -1,4 +1,4 @@
-package com.example.mylogin;
+package com.example.mylogin.Shop;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,38 +12,38 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.mylogin.Shop.ShopFrag;
+import com.example.mylogin.Frag3;
+import com.example.mylogin.R;
 
-public class Frag3 extends Fragment {
+public class ShopFrag extends Fragment {
 
     private View view;
-    private TextView tv_sample;
-    private Button btn_shop;
+    private TextView tv_sample2;
+    private Button btn_shop2;
     private String result;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.frag3, container, false);
+        view = inflater.inflate(R.layout.shopfrag, container, false);
 
-        tv_sample = view.findViewById(R.id.tv_sample);
-        btn_shop = view.findViewById(R.id.btn_shop);
+        tv_sample2 = view.findViewById(R.id.tv_sample2);
+        btn_shop2 = view.findViewById(R.id.btn_shop2);
 
         if (getArguments() != null) {
-            result = getArguments().getString("fromShopFrag");
-            tv_sample.setText(result);
+            result = getArguments().getString("fromFrag3");
+            tv_sample2.setText(result);
         }
 
-
-        btn_shop.setOnClickListener(new View.OnClickListener() {
+        btn_shop2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
-                bundle.putString("fromFrag3","거래글 작성");
+                bundle.putString("fromShopFrag","거래글 목록 표시");
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                ShopFrag shopFrag = new ShopFrag();
-                shopFrag.setArguments(bundle);
-                transaction.replace(R.id.main_frame, shopFrag);
+                Frag3 frag3 = new Frag3();
+                frag3.setArguments(bundle);
+                transaction.replace(R.id.main_frame, frag3);
                 transaction.commit();//저장
             }
         });
