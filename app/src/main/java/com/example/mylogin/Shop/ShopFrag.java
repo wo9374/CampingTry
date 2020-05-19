aad9b9aece58a03de67c120bda28c78ac9113559
june <hwijune4011@gmail.com>
2020-05-19 18:01:23 +0900

    200519-3번 프래그먼트 제작중
    수정 사항
    거래 제작중

--- app/src/main/java/com/example/mylogin/Shop/ShopFrag.java
+++ app/src/main/java/com/example/mylogin/Shop/ShopFrag.java
@@ -1,10 +1,19 @@
 package com.example.mylogin.Shop;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,11 +24,23 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.mylogin.Frag3;
 import com.example.mylogin.R;
 
-public class ShopFrag extends Fragment {
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class ShopFrag extends Fragment implements View.OnClickListener {
+    private static final int PICK_FROM_CAMERA = 0;
+    private static final int PICK_FROM_ALBUM = 1;
+    private static final int CROP_FROM_iMAGE = 2;
+    private static final int RESULT_OK = 111;
 
     private View view;
+    private  int id_view;
     private TextView tv_sample2;
     private Button btn_shop2;
+    private Uri mImageCaptureUri;
+    private ImageView iv_UserPhoto;
+    private String absoultePath;
     private String result;
 
     @Nullable
@@ -27,13 +48,13 @@ public class ShopFrag extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.shopfrag, container, false);
 
-        tv_sample2 = view.findViewById(R.id.tv_sample2);
+//        tv_sample2 = view.findViewById(R.id.tv_sample2);
         btn_shop2 = view.findViewById(R.id.btn_shop2);
 
-        if (getArguments() != null) {
-            result = getArguments().getString("fromFrag3");
-            tv_sample2.setText(result);
-        }
+//        if (getArguments() != null) {
+//            result = getArguments().getString("fromFrag3");
+//            tv_sample2.setText(result);
+//        }
 
         btn_shop2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,4 +71,152 @@ public class ShopFrag extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onClick(View v) {
+        id_view = v.getId();
+        if(v.getId() == R.id.sell_gell) {
+
+            DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakePhotoAction();
+                }
+
+
+            };
+            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    doTakeAlbumAction();
+                }
+
+
+            };
+            DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            };
+            /*new AlertDialog.Builder(this)
+                    .setTitle("업로드할 이미지 선택")
+                    .setPositiveButton("사진촬영", cameraListener)
+                    .setNeutralButton("앨범선택", albumListener)
+                    .setNegativeButton("취소", cancelListener)
+                    .show();*/
+        }
+    }
+    private void doTakePhotoAction() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+        startActivityForResult(intent, PICK_FROM_CAMERA);
+    }
+    private void doTakeAlbumAction() {
+        // 앨범 호출
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode)
+        {
+            case PICK_FROM_ALBUM:
+            {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+
+                mImageCaptureUri = data.getData();
+                Log.d("SmartWheel",mImageCaptureUri.getPath().toString());
+            }
+            case PICK_FROM_CAMERA:
+            {
+                // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+                // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_iMAGE); // CROP_FROM_CAMERA case문 이동
+                break;
+            }
+            case CROP_FROM_iMAGE:
+            {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
+                if(resultCode != RESULT_OK) {
+                    return;
+                }
+                final Bundle extras = data.getExtras();
+                // CROP된 이미지를 저장하기 위한 FILE 경로
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                        "/SmartWheel/"+System.currentTimeMillis()+".jpg";
+                if(extras != null)
+                {
+                    Bitmap photo = extras.getParcelable("data"); // CROP된 BITMAP
+                    iv_UserPhoto.setImageBitmap(photo); // 레이아웃의 이미지칸에 CROP된 BITMAP을 보여줌
+                    storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
+                    absoultePath = filePath;
+                    break;
+                }
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+            }
+        }
+    }
+
+    private void storeCropImage(Bitmap photo, String filePath) {
+        // SmartWheel 폴더를 생성하여 이미지를 저장하는 방식이다.
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
+
+        if(!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+            directory_SmartWheel.mkdir();
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            /*bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+
+                    Uri.fromFile(copyFile)));*/
+
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+
 }
