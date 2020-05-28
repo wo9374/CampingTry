1d7c307f86740206c196b536e4a9205db4fa8d41
wo9374 <55440637+wo9374@users.noreply.github.com>
2020-05-28 18:40:53 +0900

    앨범로직 취소

--- app/src/main/java/com/example/mylogin/SNS/Frag1.java
+++ app/src/main/java/com/example/mylogin/SNS/Frag1.java
@@ -1,16 +1,10 @@
 package com.example.mylogin.SNS;
 
-import android.content.ClipData;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
