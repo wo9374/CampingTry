c74a3f571d7154d21f97b2a722f0efa97ff4e435
june <hwijune4011@gmail.com>
2020-06-16 22:23:36 +0900

    마이페이지 회원정보변경 제작중

--- app/src/main/java/com/example/mylogin/MyPage/MyPage.java
+++ app/src/main/java/com/example/mylogin/MyPage/MyPage.java
@@ -11,12 +11,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mylogin.MainActivity;
+import com.example.mylogin.MyPage.RequestProfile.RequestProfileActivity;
 import com.example.mylogin.R;
 
 public class MyPage extends AppCompatActivity {
 
     private TextView d_myid, d_mynic;
-    private Button d_close;
+    private String d_nic;
+    private Button m_requestpro, d_close;
 
 
     @Override
@@ -26,6 +28,8 @@ public class MyPage extends AppCompatActivity {
 
         d_myid = findViewById(R.id.d_myid);
         d_mynic = findViewById(R.id.d_mynic);
+
+        m_requestpro = findViewById(R.id.m_requestpro);
         d_close = findViewById(R.id.d_close);
 
         Intent intent = getIntent();
@@ -33,15 +37,29 @@ public class MyPage extends AppCompatActivity {
         String str2 = intent.getStringExtra("nic");
 
         d_myid.setText(str);
-        d_mynic.setText(str2);
+        d_mynic.setText("("+str2+")님 반갑습니다!");
+        d_nic = str2;
+
+
+
+        m_requestpro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyPage.this, RequestProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
         d_close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MyPage.this, MainActivity.class);
                 intent.putExtra("userID", d_myid.getText().toString());
-                intent.putExtra("userSubname", d_mynic.getText().toString());
+                intent.putExtra("userSubname", d_nic);
                 startActivity(intent);
+                finish();
             }
         });
     }
---
+++ app/src/main/java/com/example/mylogin/MyPage/RequestProfile/RequestProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.mylogin.MyPage.RequestProfile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylogin.R;
+
+public class RequestProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_profile);
+    }
+}
