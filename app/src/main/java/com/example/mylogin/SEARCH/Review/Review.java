53a57b3739e4ef661d556d8301c0ab3f8f86ebf1
wo9374 <55440637+wo9374@users.noreply.github.com>
2020-06-18 12:01:55 +0900

    리뷰페이지,홀더(별점 레이팅바)

---
+++ app/src/main/java/com/example/mylogin/SEARCH/Review/Review.java
@@ -0,0 +1,18 @@
+package com.example.mylogin.SEARCH.Review;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mylogin.R;
+
+public class Review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+
+    }
+}
